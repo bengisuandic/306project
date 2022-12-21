fda2fc1b060a378c16a2f9e15598018ca44d6352
Bengisu Andic <bengisuandic@sabanciuniv.edu>
2022-12-21 21:56:07 +0300

    ayyh bittim

--- selection/phpFiles/select_venue.php
+++ selection/phpFiles/select_venue.php
@@ -1,3 +1,29 @@
+<html>
+<head>
+<title>...</title>
+<style type="text/css">
+table {
+margin: 50px;
+}
+
+th {
+font-family: Arial, Helvetica, sans-serif;
+font-size: 2em;
+background: #9919d0;
+color: #FFF;
+padding: 12px 16px;
+border-collapse: separate;
+border: 1px solid #000;
+}
+
+td {
+font-family: Arial, Helvetica, sans-serif;
+font-size: 2em;
+border: 1px solid #DDD;
+}
+</style>
+</head>
+<body>
 <?php 
 include "../../config1.php"; // Makes mysql connection
 
@@ -8,21 +34,25 @@ if (!empty($_POST['name'])){
     
     $sql_statement = "SELECT * FROM venues WHERE  name = '$name'";
     $result = mysqli_query($db, $sql_statement);
-    
-} 
-else 
-{
-    echo "You did not enter a name";
-}
 
-if($result)
-{
+    echo "<table>";
+    echo "<tr><th>ID</th><th>Name</th><th>Phone</th><th>Address</th></tr>";
     while($row = mysqli_fetch_assoc($result)) { // Iterating the result
         $veid = $row['veid'];
         $name = $row['name']; 
         $mobile = $row['mobile']; 
         $address = $row['address']; 
-        echo $veid . " " . $name . " " . $address . "<br>"; 
+        echo "<tr onmouseover=\"hilite(this)\" onmouseout=\"lowlite(this)\"><td>$veid</td><td>$name</td><td>$mobile</td><td>$address</td></tr>\n";
+    } 
+    echo "</table>";
+        
 } 
+else 
+{
+    echo "You did not enter a type";
 }
+
+
 ?>
+</body>
+</html>
--- selection/phpFiles/select_video.php
+++ selection/phpFiles/select_video.php
@@ -1,3 +1,29 @@
+<html>
+<head>
+<title>...</title>
+<style type="text/css">
+table {
+margin: 50px;
+}
+
+th {
+font-family: Arial, Helvetica, sans-serif;
+font-size: 2em;
+background: #9919d0;
+color: #FFF;
+padding: 12px 16px;
+border-collapse: separate;
+border: 1px solid #000;
+}
+
+td {
+font-family: Arial, Helvetica, sans-serif;
+font-size: 2em;
+border: 1px solid #DDD;
+}
+</style>
+</head>
+<body>
 <?php 
 include "../../config1.php"; // Makes mysql connection
 
@@ -8,21 +34,25 @@ if (!empty($_POST['name'])){
     
     $sql_statement = "SELECT * FROM videos WHERE  name = '$name'";
     $result = mysqli_query($db, $sql_statement);
-    
-} 
-else 
-{
-    echo "You did not enter a name";
-}
 
-if($result)
-{
+    echo "<table>";
+    echo "<tr><th>ID</th><th>Name</th><th>Link</th><th>Description</th></tr>";
     while($row = mysqli_fetch_assoc($result)) { // Iterating the result
         $viid = $row['viid'];
         $name = $row['name']; 
         $link = $row['link']; 
         $description = $row['description']; 
-        echo $viid . " " . $name . " " . $link . "<br>"; 
+        echo "<tr onmouseover=\"hilite(this)\" onmouseout=\"lowlite(this)\"><td>$viid</td><td>$name</td><td>$link</td><td>$description</td></tr>\n";
+    } 
+    echo "</table>";
+        
 } 
+else 
+{
+    echo "You did not enter a type";
 }
+
+
 ?>
+</body>
+</html>
