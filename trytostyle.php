ffca55378a37a2e3935bfba9e298fa353933100e
Bengisu Andic <bengisuandic@sabanciuniv.edu>
2022-12-21 21:14:23 +0300

    sad

--- trytostyle.php
+++ trytostyle.php
@@ -25,7 +25,7 @@ border: 1px solid #DDD;
 </head>
 <body>
 <?php 
-include "../../config1.php"; // Makes mysql connection
+include "config1.php"; // Makes mysql connection
 
 
 if (!empty($_POST['type'])){ 
@@ -34,26 +34,28 @@ if (!empty($_POST['type'])){
     
     $sql_statement = "SELECT * FROM events WHERE  type = '$type'";
     $result = mysqli_query($db, $sql_statement);
-    
+
+    echo "<table>";
+    echo "<tr><th>ID</th><th>hashtag</th></tr>";
+    while($row = mysqli_fetch_assoc($result)) { // Iterating the result
+        $eid = $row['eid'];
+        $name = $row['name']; 
+        $type = $row['type'];
+        $price = $row['price']; 
+        $capacity = $row['capacity']; 
+        $date = $row['date']; 
+        //echo $eid . " " . $name . " " . $price . "<br>"; 
+        echo "<tr onmouseover=\"hilite(this)\" onmouseout=\"lowlite(this)\"><td>$eid</td><td>$name</td><td>$price</td></tr>\n";
+    } 
+    echo "</table>";
+        
 } 
 else 
 {
     echo "You did not enter a type";
 }
 
-echo "<table>";
-echo "<tr><th>ID</th><th>hashtag</th></tr>";
-while($row = mysqli_fetch_assoc($result)) { // Iterating the result
-    $eid = $row['eid'];
-    $name = $row['name']; 
-    $type = $row['type'];
-    $price = $row['price']; 
-    $capacity = $row['capacity']; 
-    $date = $row['date']; 
-    //echo $eid . " " . $name . " " . $price . "<br>"; 
-    echo "<tr onmouseover=\"hilite(this)\" onmouseout=\"lowlite(this)\"><td>$eid</td><td>$name</td><td>$price</td></tr>\n";
-} 
-echo "</table>";
+
 ?>
 </body>
 </html>
